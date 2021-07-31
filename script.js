278382b5d672a30789e257a3143c98cb36114aab
kevaf <kevinkipyegon20@gmail.com>
2021-07-31 14:23:19 +0300

    add validation to form inputs

--- script.js
+++ script.js
@@ -14,14 +14,25 @@ $("#topp").click(function(){
     $('#pizzasize').hide();
     $('#crust').hide();
 })
+});
 
 function orderOnline(){
-    var size = document.getElementById("crust1").value;
+    var size = document.getElementById("size").value;
     var topping =document.getElementById("toppings1").value;
     var crust =document.getElementById("crust1").value;
+    var quantity=parseInt(document.getElementById("qty").value);
+
+   const pizzaSize=["Small", "Medium", "Large"];
+   const pizzaCrust=["Pizza-Bagels", "Cheese-Stuffed", "Flatbread", "Crispy", "Gluten-Free"];
+   const pizzaToppings=["Pepperoni", "Mushrooms", "Onions", "Sausage", "Extra-Cheese"];
+   
+   if(size=="" || topping=="" ||crust=="" || document.getElementById("qty").value.length==0){
+       alert("Ensure All Options Are Selected to make order")
+   }
+   else if(quantity<1){
+       alert("Enter Number of Pizzas")
+   }
+   
+ 
+
 }
-
-
-
-
-});
